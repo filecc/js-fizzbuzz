af657f781d14b911a44f384f70b1538a89c55f74
filecc <fil.gia@live.it>
2023-03-12 18:31:59 +0100

    added progress bar on bottom, changed styling

--- js/index.js
+++ js/index.js
@@ -1,20 +1,62 @@
 const app = document.getElementById('app');
-let numberOfBoxs = 0;
-let toBeCreated = 1;
-const restart = document.querySelector('.restart');
 
+/* ++++++++ fixing window height on iPhone (for using 100vh) ++++++++ */
+const doc = document.documentElement;
+
+const appHeight = () => {
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+};
+window.addEventListener('resize', appHeight);
+appHeight();
+/* ++++++++ fixing window height on iPhone (for using 100vh) ++++++++ */
+
+// value form users
+let numberOfBoxs = 0;
+// variable to teke track of the created elements
+let toBeCreated = 1;
+
+const restart = document.querySelector('.restart');
+const progress = document.getElementById('progress');
+
+// main function fro creating elements
 function createItem() {
+  
+
+  /* progress bar function */
+  function move() {
+      const elem = document.getElementById("progress");
+      // the function frame is called every 10 milliseconds
+      const id = setInterval(frame, 10);
+      function frame() {
+        // take the % of the elements created
+        let itemsBuilded = (toBeCreated / numberOfBoxs ) * 100; 
+        // if the the program finish to creat stop calling frame()
+        if (itemsBuilded > 100) {
+          clearInterval(id);
+        } else { // otherwise set the width of the progress bar at the % indicated
+          elem.style.width = itemsBuilded + "%";
+        }
+      }
+    
+  }
+
+  // show the container of the boxes
   app.classList.add('show');
   const square = document.createElement('div');
   square.classList.add('box');
   square.classList.add('appearing');
+  // call move() function onliy at first box created
+  toBeCreated == 1 && move();
+  //append the first div only if is the first of the game
   toBeCreated < numberOfBoxs && app.appendChild(square);
 
+  // listen to the animationend of every box in the page
   square.addEventListener('animationend', () => {
     let text = toBeCreated;
 
+    // THE ACTUAL FIZZ BUZZ GAME
     toBeCreated % 15 === 0
-      ? ((text = 'FizzBuzz'),
+      ? ((text = 'Fizz<br>Buzz'),
         square.classList.add('fizzBuzz'),
         square.classList.add('fbAnimation'))
       : toBeCreated % 3 === 0
@@ -22,17 +64,25 @@ function createItem() {
       : toBeCreated % 5 === 0 &&
         ((text = 'Buzz'), square.classList.add('buzz'));
 
+        // append the text to the box after creating them
     const display = document.createElement('span');
     square.appendChild(display);
     display.innerHTML = text;
 
+    // calling the function
     createItem();
+    // element created
     toBeCreated += 1;
+    
+    // if complete show the restart button
     if (toBeCreated == numberOfBoxs) {
       restartBox.classList.toggle('hidden');
     }
   });
+  
 }
+
+
 
 const inputBox = document.querySelector('.inputBox');
 const input = document.querySelector('input[name="numberBox');
@@ -41,11 +91,13 @@ const restartBox = document.querySelector('.restartBox');
 
 const start = document.querySelector('.start');
 
+// button for starting the game
 start.addEventListener('click', () => {
   start.classList.toggle('hidden');
   inputBox.classList.toggle('hidden');
   play.classList.toggle('hidden');
 
+  // input validation
   input.addEventListener('input', () => {
     if (input.value > 150) {
       input.value = 150;
@@ -57,6 +109,8 @@ start.addEventListener('click', () => {
       play.classList.remove('hidden');
     }
   });
+
+
 
   play.addEventListener('click', () => {
     numberOfBoxs = input.value;
@@ -92,11 +146,3 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () 
       changeColor();
     });
 
-/* fixing window height on iPhone */
-
-const appHeight = () => {
-  const doc = document.documentElement
-  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
